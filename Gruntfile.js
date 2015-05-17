e6a1aee6b941c445b12726c69da91d2cd5e1e5c9
Marcos Iglesias <miglesias@eventbrite.com>
2015-05-17 16:13:29 -0700

    Adding build system

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+    // Initialize the Grunt configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Configure the concat task
+        concat: {
+            js: {
+                src: [
+                    'src/start.js',
+                    'src/chart/bar.js',
+                    'src/layout/layout.js',
+                    'src/end.js'
+                ],
+                dest: 'dist/codementor.js'
+            }
+        },
+
+        // Uglify Configuration
+        uglify: {
+            options: {
+                mangle: false
+            },
+            js: {
+                files: {
+                    'dist/codementor.min.js': ['dist/codementor.js']
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['concat', 'uglify']);
+
+};
+
