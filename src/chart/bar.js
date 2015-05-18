db01761cde89b29e0c20a99920f82731633d65b9
Marcos Iglesias <miglesias@eventbrite.com>
2015-05-17 19:37:28 -0700

    Adding SVG element test

--- src/chart/bar.js
+++ src/chart/bar.js
@@ -1,7 +1,26 @@
 define(function(require){
     'use strict';
 
+    var d3 = require('d3'),
+        data,
+        svg;
 
+    return function module(){
+
+        function exports(_selection){
+            _selection.each(function(_data){
+                data = _data;
+
+                if (!svg) {
+                    svg = d3.select(this)
+                        .append('svg')
+                        .classed('bar-chart', true);
+                }
+            });
+        }
+
+        return exports;
+    };
 
 
 });
