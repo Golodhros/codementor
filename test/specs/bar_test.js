0c22a1cdd71b28ba386dc24c9a45be53c65d6dad
Marcos Iglesias <miglesias@eventbrite.com>
2015-05-17 19:37:28 -0700

    Adding SVG element test

--- test/specs/bar_test.js
+++ test/specs/bar_test.js
@@ -9,6 +9,114 @@ define(['jquery', 'd3', 'src/chart/bar'], function($, d3, Chart) {
             containerFixture = d3.select('.test-container');
             f.load('testContainer.html');
 
+            dataset = [
+                {
+                    letter: 'A',
+                    frequency: .08167
+                },
+                {
+                    letter: 'B',
+                    frequency: .01492
+                },
+                {
+                    letter: 'C',
+                    frequency: .02782
+                },
+                {
+                    letter: 'D',
+                    frequency: .04253
+                },
+                {
+                    letter: 'E',
+                    frequency: .12702
+                },
+                {
+                    letter: 'F',
+                    frequency: .02288
+                },
+                {
+                    letter: 'G',
+                    frequency: .02015
+                },
+                {
+                    letter: 'H',
+                    frequency: .06094
+                },
+                {
+                    letter: 'I',
+                    frequency: .06966
+                },
+                {
+                    letter: 'J',
+                    frequency: .00153
+                },
+                {
+                    letter: 'K',
+                    frequency: .00772
+                },
+                {
+                    letter: 'L',
+                    frequency: .04025
+                },
+                {
+                    letter: 'M',
+                    frequency: .02406
+                },
+                {
+                    letter: 'N',
+                    frequency: .06749
+                },
+                {
+                    letter: 'O',
+                    frequency: .07507
+                },
+                {
+                    letter: 'P',
+                    frequency: .01929
+                },
+                {
+                    letter: 'Q',
+                    frequency: .00095
+                },
+                {
+                    letter: 'R',
+                    frequency: .05987
+                },
+                {
+                    letter: 'S',
+                    frequency: .06327
+                },
+                {
+                    letter: 'T',
+                    frequency: .09056
+                },
+                {
+                    letter: 'U',
+                    frequency: .02758
+                },
+                {
+                    letter: 'V',
+                    frequency: .00978
+                },
+                {
+                    letter: 'W',
+                    frequency: .02360
+                },
+                {
+                    letter: 'X',
+                    frequency: .00150
+                },
+                {
+                    letter: 'Y',
+                    frequency: .01974
+                },
+                {
+                    letter: 'Z',
+                    frequency: .00074
+                }
+            ];
+            barChart = Chart();
+            containerFixture.datum(dataset).call(barChart);
         });
 
         afterEach(function(){
@@ -16,6 +124,10 @@ define(['jquery', 'd3', 'src/chart/bar'], function($, d3, Chart) {
             f = jasmine.getFixtures();
             f.cleanUp();
             f.clearCache();
+        });
+
+        it('should render a chart with minimal requirements', function(){
+            expect(containerFixture.select('.bar-chart')).toBeDefined(1);
         });
 
     });
